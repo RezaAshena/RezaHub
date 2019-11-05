b6f33c980acac5e0f9800d7424b440040daf4aca
RezaAshena <ashena.reza@gmail.com>
2019-11-05 04:55:42 -0500

    Create a folder and name it models , Add the Group, User and Context models

---
+++ Models/user.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RezaHub.Models
+{
+    public class user
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        // from the group model (Entity framework will connect the Primarykey and forign key)
+        public group Group { get; set; }
+        public int GroupId { get; set; }
+    }
+}
